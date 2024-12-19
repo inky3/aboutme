c9ab06f4a29b09bc4fd1010f9e9b69f44f898c73
unknown <Boom05@windowslive.com>
2024-12-19 18:19:20 +0700

    fix a few thing?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
-export default {
-  site: "https://github.com/inky3/portfolio", // Use your actual GitHub Pages URL
-  build: {
-    outDir: "./dist",
-  },
-  vite: {
-    base: "/portfolio/",  // The subpath of your repository, if applicable
-  },
-};
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  base: '/portfolio/', // Astro adjusts all paths for you
+});
